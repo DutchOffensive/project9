580d069a080c708293df8498347cc287aaced252
DutchOffensive <52233154+DutchOffensive@users.noreply.github.com>
2020-09-06 21:20:09 +0200

    Update details.php

--- details.php
+++ details.php
@@ -5,6 +5,7 @@
     </head>
     <body>
     <a href="bachstraat.php"><button class="btn btn-danger">Terug</button></a>
+    <h1>Gegevens Bijwerken</h1>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -36,7 +37,7 @@ while($row = mysqli_fetch_assoc($query)){
       $email = $array["email"];
       $phone = $array["phone"];
       $covid = $array["covid"];
-    echo "<div class='container'>" .
+    echo "<div class='container-lg center'>" .
     "<form action='<?php echo htmlspecialchars($_SERVER[PHP_SELF]); ?>' method='post'>" .
      "<div class='form-row'>" .
     "<div class='col-md-6 mb-3'>" .
