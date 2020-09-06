5507ff97391dd8efb71f4430a2ca0caa7fe8fe73
DutchOffensive <52233154+DutchOffensive@users.noreply.github.com>
2020-09-06 16:41:31 +0200

    Update bachstraat.php

--- bachstraat.php
+++ bachstraat.php
@@ -5,22 +5,27 @@
     </head>
     <body>
 
+<a href="home.php"><button class="btn btn-danger">Terug</button></a>
         <h1>ROC Rivor</h1><br>
 
-
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-
-<div class="container">   
+<div class="container-lg">
 <table class='table table-striped'>
+<!-- <thead class="bigger-font">
+    <tr>
+      <th scope="col">Firstname</th>
+      <th scope="col">Lastname</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Time</th>
+    </tr>
+  </thead> -->
 <tbody class="bigger-font">
-    <td><p>Firstname</p></td>
-    <td><p>Lastname</p> </td>  
-    <td><p>Phone</p></td>  
-    <td><p>Time</p></td>   
+    <td scope="col"><p>Firstname</p></td>
+    <td scope="col"><p>Lastname</p> </td>  
+    <td scope="col"><p>Phone</p></td>  
+    <td scope="col"><p>Time</p></td>   
 </tbody>
-</table>    
+</table>   
+</div>
 <?php
 
 require_once "config.php";
@@ -41,15 +46,20 @@ while($row = mysqli_fetch_assoc($query)){
     array_push($rows, $row);
   }
   foreach ($rows as $key => $array) {
+    echo "<div class='container-lg'>"; 
     echo "<table class='table table-striped'>"; 
     echo "<tbody>";
-    echo "<td>" . $array["firstname"] . "</td>" . "<td>" . $array["lastname"] . "</td>" . "<td>" . $array["phone"] . "</td>" . "<td>" . $array["timestamp"] . "</td>";
+    echo "<td scope='col'>" . $array["firstname"] . "</td>" . "<td scope='col'>" . $array["lastname"] . "</td>" . "<td scope='col'>" . $array["phone"] . "</td>" . "<td scope='col'>" . $array["timestamp"] . "</td>";
     echo "</tbody>";
     echo "</table>";
+    echo "</div>";
   }
 ?>
 </div>
 
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </body>
 </html>
 
