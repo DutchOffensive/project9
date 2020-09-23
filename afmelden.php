dc7d719ae6254199693bb11fcfc16adf908a1594
youriverbon <52278592+youriverbon@users.noreply.github.com>
2020-09-23 23:31:54 +0200

    Updated query and added error handling.

--- afmelden.php
+++ afmelden.php
@@ -1,7 +1,18 @@
 <?php
 require_once "config.php";
-$id= $_GET['id'];
-$sql = "UPDATE `guests` SET `time_left`='' WHERE 'id_guests' = $id";
+$id = $_GET['id'];
+
+$time_left = date("Y-m-d H:i:s");
+
+$sql = "UPDATE `guests` SET `time_left`='$time_left' WHERE `id_guests` = $id";
 mysqli_query($link,$sql);
 
+$affected_rows = mysqli_affected_rows($link);
+
+if($affected_rows == 0){
+    echo"<h1>Helaas, er is iets mis gegaan. Probeer het nogmaals.</h1>";
+    die;
+}
+
+header("location: bedankt.php");
 ?>
